98ffe314679d7140c0b4404979d0a38eb6f64284
Andrew Owenson <a.m.b.owenson@warwick.ac.uk>
2020-09-04 18:01:43 +0100

    New indirect_rw() kernel to prevent intra-iteration SIMD

--- src/Kernels/indirect_rw_kernel.h
+++ src/Kernels/indirect_rw_kernel.h
@@ -80,20 +80,21 @@ inline void indirect_rw_kernel(
     #endif
 
     double p_a_val  = p_b + ex;
-    double pe_a_val = pe_b + ey;
-    double mx_a_val = momentum_b.x + ez;
-    double my_a_val = momentum_b.y;
-    double mz_a_val = momentum_b.z;
+    double mx_a_val = momentum_b.y * momentum_b.z + ez;
+    double my_a_val = momentum_b.x * momentum_b.z;
+    double mz_a_val = momentum_b.x + momentum_b.y;
+    double pe_a_val = pe_b - ey;
+    
     #ifdef FLUX_PRECOMPUTE_EDGE_WEIGHTS
         double p_b_val = p_a + ewt;
         #else
-        double p_b_val = p_a;
+        double p_b_val = p_a + ey;
     #endif
-    double pe_b_val = pe_a;
-    double mx_b_val = momentum_a.x;
-    double my_b_val = momentum_a.y;
-    double mz_b_val = momentum_a.z;
-
+    double mx_b_val = momentum_a.y + momentum_a.z * ez;
+    double my_b_val = momentum_a.x * momentum_a.z;
+    double mz_b_val = momentum_a.x + momentum_a.y;
+    double pe_b_val = pe_a - ey;
+    
     // Write out fluxes to memory:
     #ifdef FLUX_FISSION
         edge_variables[VAR_DENSITY       ].a =  p_a_val;
